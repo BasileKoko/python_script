775815dd3715a6bf6f9c44e76719d592153d27b6
Basile Koko <basile_koko@hotmail.com>
2017-06-16 21:20:34 +0100

    adding method get_username

--- favourite_language.py
+++ favourite_language.py
@@ -3,3 +3,6 @@ from dotenv import Dotenv
 class FavouriteLanguage:
     end_point = 'https://api.github.com/users/'
     token = "?access_token=" + Dotenv('./.env')['TOKEN']
+
+    def get_username(self):
+        username = raw_input("Please enter Github username: ")
