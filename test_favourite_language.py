9b0d9a79048320f726092af51c87428d81662d52
Basile Koko <basile_koko@hotmail.com>
2017-06-24 23:27:32 +0100

    refactoring test

--- test_favourite_language.py
+++ test_favourite_language.py
@@ -15,31 +15,26 @@ def instance():
     return instance
 
 def test_validate_username(capsys, instance):
-    instance
     instance.validate_username("")
     output(capsys)
     assert output(capsys) == "Sorry username was not found\n"
 
 def test_username_with_repositories_language(capsys, instance):
-    instance
     instance.validate_username("2")
     output(capsys)
     assert output(capsys) == "HTML\n"
 
 def test_get_repositories(capsys, instance):
-    instance
     instance.get_repositories("testfortest2017")
     output(capsys)
     assert output(capsys) == "Sorry no repositories found for testfortest2017\n"
 
 def test_get_repositories_language(capsys, instance):
-    instance
     instance.get_repositories_language([])
     output(capsys)
     assert output(capsys) == "Sorry no language found in the user repositories\n"
 
 def test_get_favourite_language(capsys, instance):
-    instance
     instance.get_favourite_language(["Python", "Ruby", "Python"])
     output(capsys)
     assert output(capsys) == "Python\n"
